8c00fddc4d48ebc22849f43ca6337e560826aa46
Hcosse <hcosse02@gmail.com>
2025-10-23 15:51:51 +0200

    update osteoweb title link

---
+++ runner.py
@@ -0,0 +1,4 @@
+from scrapers.osteoweb_fr import get_info_remplacement
+
+if __name__ == "__main__":
+    get_info_remplacement()
--- scrapers/osteoweb_fr.py
+++ scrapers/osteoweb_fr.py
@@ -1,7 +1,42 @@
-import os
 import requests
-from bs4 import BeautifulSoup   
-from dotenv import load_dotenv  
-import time
+from bs4 import BeautifulSoup
 
 
+def get_remplacement_links():
+    url = "https://www.osteoweb.fr/remplacement/"
+    r = requests.get(url)
+    soup = BeautifulSoup(r.text, "html.parser")
+
+    liens = set()
+
+    for a in soup.select('a[href^="https://www.osteoweb.fr/"]'):
+        href = a.get("href")
+        if href and "remplacement" in href and href.endswith(".htm"):
+            liens.add(href)
+
+    return list(liens)
+
+
+
+
+ 
+def get_info_remplacement():
+    liens  = get_remplacement_links()
+
+    data = []
+
+    for lien in liens:
+        r = requests.get(lien)
+        soup = BeautifulSoup(r.text, "html.parser")
+
+        titre = soup.find("title").get_text()
+        
+        
+        data.append({
+            "titre": titre,
+            "lien": lien,
+        })
+    return data
+
+
+    get_info_remplacement() 
