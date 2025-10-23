7f78e526c5155827e6b9901a5e6991917671e8e5
Hcosse <hcosse02@gmail.com>
2025-10-23 12:17:06 +0200

    first SSH push

--- scrapers/osteoweb_fr.py
+++ scrapers/osteoweb_fr.py
@@ -1,28 +1,7 @@
-import yaml
+import os
 import requests
-from bs4 import BeautifulSoup
-import yaml
-import os 
-import dotenv
-
-dotenv.load_dotenv()
+from bs4 import BeautifulSoup   
+from dotenv import load_dotenv  
+import time
 
 
-def scrape_osteoweb():
-    url = os.getenv('BASE_URL_OSTEOWEB')
-    print(url)
-    r = requests.get(url)
-    soup = BeautifulSoup(r.text, "html.parser")
-
-    annonces = []
-    for article in soup.select("article"):
-        titre = article.select_one("h2").get_text(strip=True)
-        ville = article.select_one(".ville").get_text(strip=True)
-        lien = article.select_one("a")["href"]
-        annonces.append({
-            "source": "osteoweb.fr",
-            "titre": titre,
-            "ville": ville,
-            "lien_annonce": lien
-        })
-    return annonces
